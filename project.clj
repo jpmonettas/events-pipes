1f5287a1cf76c9c7cf3e9d82c4b17021454b8450
Juan Monetta <jpmonettas@gmail.com>
2015-09-11 13:05:28 -0300

    Preparing 0.10.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject events-pipes "0.10.0"
+(defproject events-pipes "0.10.1"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
